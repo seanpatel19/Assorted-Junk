eb60e4c2326dd99d7454f1685e9c1d3c58217a82
sean <seanpatel076@gmail.com>
2021-09-08 00:06:05 -0400

    list of nfl teams

---
+++ football.py
@@ -0,0 +1,50 @@
+week_one = ["PackersSaints","seahawkscolts","jetspanthers","berasrams","browns chiefs","steelers bills","cards titans","49ers lions","eagles falcons","vikings bengals","broncos giants", "cowboys bucs", "jax houston","chargers wash","dolphins pats","ravens raiders"]
+###previous line is not as efficent as could be 
+NFL_Teams = [
+"SEA",
+"LAR",
+"ARI",
+"SF",
+"NO",
+"TB",
+"CAR",
+"ATL",
+"GB",
+"CHI",
+"MIN",
+"DET",
+"WASH",
+"PHI",
+"NYG",
+"DAL",
+"KC",
+"LV",
+"LAC",
+"DEN",
+"TENN",
+"IND",
+"HOU",
+"JAX",
+"PITT",
+"BAL",
+"CLE",
+"CIN",
+"BUF",
+"NE",
+"NYJ",
+"MIA"]
+
+print(NFL_Teams)
+
+nfc=NFL_Teams[0:16]
+afc=NFL_Teams[16:32]
+nfc_west = NFL_Teams[0:4]
+nfc_south = NFL_Teams[4:8]
+nfc_north = NFL_Teams[8:12]
+nfc_east = NFL_Teams[12:16]
+afc_west = NFL_Teams[16:20]
+afc_south =NFL_Teams[20:24]
+afc_north =NFL_Teams[24:28]
+afc_east= NFL_Teams[28:32]
+
+print(afc)
